4e43283993a559477f2b5c41d9882b6bda1852db
xyxc0673 <xyxc0673@gmail.com>
2018-11-29 12:19:03 +0800

    新增 关于页面的开源卡片

--- src/pages/common/setting/about.tsx
+++ src/pages/common/setting/about.tsx
@@ -24,7 +24,7 @@ export default class About extends Component {
 
   componentDidHide () { }
 
-  onCopyTextClick (text) {
+  handleCopyTextClick (text) {
     Taro.setClipboardData({data: text})
   }
 
@@ -39,13 +39,16 @@ export default class About extends Component {
             <View className='intro-item'>[吉珠小助手]小程序为吉林大学珠海学院学生提供教务系统、校园卡、图书馆查询等服务。</View>
             <View className='intro-item'>如果你在使用的过程中遇到了问题或者有什么想法，欢迎通过关于页面的反馈选项告诉我。</View>
           </View>
-          <Panel title='更新日志'>
+          <Panel title='更新日志' marginBottom={0}>
             <View className='list'>
               <View className="list-item title">版本: 0.0.1 日期: 2018-11-26</View>
               <View className='list-item'>小程序初版完成</View>
             </View>
-          </Panel>  
-          <Panel title='感谢'>
+          </Panel>
+          <Panel title='开源' marginBottom={0}>
+            <View className='copy-text' onClick={this.handleCopyTextClick.bind(this, 'http://t.cn/ELtmhvK')}>http://t.cn/ELtmhvK</View>
+          </Panel>
+          <Panel title='感谢' marginBottom={0}>
             <View className='list'>
               <View className='list-item'>Taro</View>
               <View className='list-item'>Iconfont</View>
